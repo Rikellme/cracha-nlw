659fe9022e3364b206a14f166780098563db03af
Rikellme Keven Andrade de Lima <rikellmekevin@gmail.com>
2021-12-30 14:00:47 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,33 @@
+const linksSocialMedia = {
+  github: 'Rikellme',
+  youtube: 'UCOiudw-2yvGbW-KHqh5ltlA',
+  facebook: 'rikellme.kevin',
+  instagram: 'rikellme_kevin',
+  twiter: ''
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${linksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
